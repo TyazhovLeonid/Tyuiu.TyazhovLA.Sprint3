04be3f71ecd3116ef18e85cc2727f095ed74023a
Tyuiu.TyazhovLA@outlook.com <99raindeath@gmail.com>
2024-09-19 19:45:48 +0500

    Task4

---
+++ Tyuiu.TyazhovLA.Sprint3.Task4.V16.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.TyazhovLA.Sprint3.Task4.V16.Lib;
+namespace Tyuiu.TyazhovLA.Sprint3.Task4.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = -5; int stopValue=5; 
+            double res = ds.Calculate(startValue, stopValue);
+            Assert.AreEqual(res,res);
+        }
+    }
+}
---
+++ Tyuiu.TyazhovLA.Sprint3.Task4.V16/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.TyazhovLA.Sprint3.Task4.V16.Lib;
+namespace Tyuiu.TyazhovLA.Sprint3.Task4.V16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Тяжов Л. А. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Continue and Break                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнил: Тяжов Леонид Александрович | ПКТб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue=Convert.ToInt32(Console.ReadLine());
+            int stopValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(startValue, stopValue));
+            Console.ReadKey();
+
+        }
+    }
+}
